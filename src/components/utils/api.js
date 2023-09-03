f7a617a0842c944ba413acd6c9027d2434409194
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-09-03 23:37:41 +0200

    Deployment Fix

--- src/components/utils/api.js
+++ src/components/utils/api.js
@@ -1,11 +1,11 @@
-// import getRiotDevAPIKey from './dev_key';
+import getRiotDevAPIKey from './dev_key';
 
 // search by summoner name to get the summonerID
 // https://developer.riotgames.com/apis#summoner-v4/GET_getBySummonerName
 export async function getInfoBySummonerName(summonerName) {
   try {
     const response = await fetch(
-      `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.RIOT_DEV_API_KEY}`,
+      `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${getRiotDevAPIKey()}`,
     );
     const summonerData = await response.json();
     return summonerData;
@@ -20,7 +20,7 @@ export async function getInfoBySummonerName(summonerName) {
 export async function getRankedInfoBySummonerId(summonerId) {
   try {
     const response = await fetch(
-      `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${process.env.RIOT_DEV_API_KEY}`,
+      `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${getRiotDevAPIKey()}`,
     );
     const rankedData = await response.json();
     return rankedData;
---
+++ src/components/utils/dev_key.js
@@ -0,0 +1,4 @@
+// key expires within 24 hours
+export default function getRiotDevAPIKey() {
+  return 'RGAPI-a79e2524-ac79-499e-a757-8d9912524fbc';
+}
