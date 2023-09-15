d2b2543d2bb5370ebb2409e01b51b724d7b69da1
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-09-15 18:37:15 +0200

    Edit png for deleteImg

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -69,7 +69,7 @@ function Icon({ isHovered, iconId, removeHandler, summonerName }) {
             className="deleteImg icon"
             draggable="false"
             onClick={handleClick}
-            src={'src/assets/minus.png'}
+            src={'https://cdn-icons-png.flaticon.com/512/1828/1828666.png'}
           />
         </>
       ) : (
