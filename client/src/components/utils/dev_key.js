d6174e7e479df22eac54c3b36b8fed38832d624c
Dallair220 <lovance777@gmail.com>
2024-02-02 14:45:41 +0100

    Create directories for client and server + setup Express to serve built React app files

--- client/src/components/utils/dev_key.js
+++ client/src/components/utils/dev_key.js
@@ -1,4 +1,4 @@
 // key expires within 24 hours
 export default function getRiotDevAPIKey() {
-  return 'RGAPI-d8506e4e-e5f9-4ca4-8963-e195c554867f';
+  return 'RGAPI-6881e722-d169-420e-a841-2b4134e9e784';
 }
