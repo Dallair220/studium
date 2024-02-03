d5be8c23786fe7f23028c869e2921f91bba824ae
Dallair220 <lovance777@gmail.com>
2024-02-03 17:17:13 +0100

    Add MongoDB connection and update static file path

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+console.log('server started');
 
+// MongoDB connection
+mongoose.set('strictQuery', false);
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.MONGODB_CONNECTION_URI);
+  console.log('database connected');
+}
+
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
