286017595f7f71675636742b5cbbe4b2bb17b6ed
Dallair220 <lovance777@gmail.com>
2024-02-03 15:22:10 +0100

    Update dev Riot API key

--- client/src/components/utils/dev_key.js
+++ client/src/components/utils/dev_key.js
@@ -1,4 +1,4 @@
 // key expires within 24 hours
 export default function getRiotDevAPIKey() {
-  return 'RGAPI-6881e722-d169-420e-a841-2b4134e9e784';
+  return 'RGAPI-04dd0b59-1fdf-4683-813b-1c658da56200';
 }
