a14c7f9ac90d2004bc9c991662edd45994f20bb2
Dallair220 <lovance777@gmail.com>
2024-02-15 11:35:43 +0100

    Add summoner data and update player creation and update

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 const sortLadder = require('../utils/rankSortingUtils');
+const bundleInfoBySummonerName = require('../utils/summonerDataUtils');
 const Player = require('../models/player');
 const Rank = require('../models/rank');
 
@@ -22,28 +23,53 @@ exports.player_create_post = [
     const errors = validationResult(req);
     // If there are errors, send them to the client.
     if (!errors.isEmpty()) {
-      res.json({ status: 'error', errors: errors.array() });
+      res.json({ status: 'error', message: errors.array() });
       return;
     }
-    // Create a Player object with escaped and trimmed data.
-    const newPlayer = new Player({
-      gameName: req.body.gameName,
-      tagLine: req.body.tagLine,
+    // Get the summoner's rank information.
+    const summonerData = await bundleInfoBySummonerName(req.body.gameName);
+    // Check if the player already exists in the database.
+    const playerExists = await Player.findOne({ gameName: summonerData.gameName });
+    if (playerExists) {
+      res.json({ status: 'error', message: 'Player already exists.' });
+      return;
+    }
+    // Create a Rank object
+    const rank = new Rank({
+      rank: summonerData.soloRank.tier,
+      division: summonerData.soloRank.rank,
+      leaguePoints: summonerData.soloRank.leaguePoints,
+      wins: summonerData.soloRank.wins,
+      losses: summonerData.soloRank.losses,
     });
-    // Save the new Player to the database and send it to the client.
+    await rank.save();
+    // Create a Player object
+    const newPlayer = new Player({
+      gameName: summonerData.gameName,
+      tagLine: req.body.tagLine,
+      profileIconId: summonerData.profileIconId,
+      rank: rank,
+    });
     await newPlayer.save();
-    res.json({ status: 'success', newPlayer });
+    // Send successfull response to the client.
+    res.json({ status: 'success', message: 'Player created successfully.' });
   }),
 ];
 
 // Handle Player delete on POST.
 exports.player_delete_post = asyncHandler(async (req, res, next) => {
-  const existingPlayer = await Player.findById(req.params.id); // or req.body.playerId
+  const existingPlayer = await Player.findById(req.params.id);
   if (existingPlayer === null) {
     res.json({ status: 'error', message: 'Player not found.' });
     return;
   }
-  await existingPlayer.remove();
+  const existingRank = await Rank.findById(existingPlayer.rank);
+  if (existingRank === null) {
+    res.json({ status: 'error', message: 'Rank not found.' });
+    return;
+  }
+  await Player.findByIdAndDelete(req.params.id);
+  await Rank.findByIdAndDelete(existingPlayer.rank);
   res.json({ status: 'success', message: 'Player deleted successfully.' });
 });
 
@@ -58,17 +84,36 @@ exports.player_update_post = [
     const errors = validationResult(req);
     // If there are errors, send them to the client.
     if (!errors.isEmpty()) {
-      res.json({ status: 'error', errors: errors.array() });
+      res.json({ status: 'error', message: errors.array() });
       return;
     }
+    // Get the summoner's rank information.
+    const summonerData = await bundleInfoBySummonerName(req.body.gameName);
+    // Check if the player already exists in the database.
+    const playerExists = await Player.findOne({ gameName: summonerData.gameName });
+    if (playerExists) {
+      res.json({ status: 'error', message: 'Player already exists.' });
+      return;
+    }
+    // Create a Rank object
+    const updatedRank = new Rank({
+      rank: summonerData.soloRank.tier,
+      division: summonerData.soloRank.rank,
+      leaguePoints: summonerData.soloRank.leaguePoints,
+      wins: summonerData.soloRank.wins,
+      losses: summonerData.soloRank.losses,
+      _id: playerExists.rank, // ?
+    });
+    await Rank.findByIdAndUpdate(playerExists.rank, updatedRank, {});
     // Create a Player object with escaped and trimmed data.
     const updatedPlayer = new Player({
-      gameName: req.body.gameName,
+      gameName: summonerData.gameName,
       tagLine: req.body.tagLine,
+      profileIconId: summonerData.profileIconId,
       _id: req.params.id,
     });
     // Save the updated Player to the database and send it to the client.
     await Player.findByIdAndUpdate(req.params.id, updatedPlayer, {});
-    res.json({ status: 'success', updatedPlayer });
+    res.json({ status: 'success', message: 'Player updated successfully.' });
   }),
 ];
