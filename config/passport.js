1b40fb67bd5bcc6372b111a17d0536d0168496f3
Dallair220 <lovance777@gmail.com>
2024-02-28 09:42:22 +0100

    Refactor passport.js local strategy

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,7 @@
-const bcrypt = require('bcryptjs');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
-
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-    async (email, password, done) => {
-      try {
-        const user = await User.findOne({ email });
-        if (!user) {
-          return done(null, false, { message: 'Incorrect email' });
-        }
-        // Check if the password is correct
-        const pwIsValid = await bcrypt.compare(password, user.password);
-        if (!pwIsValid) {
-          return done(null, false, { message: 'Incorrect password' });
-        }
-        return done(null, user);
-      } catch (err) {
-        return done(err);
-      }
-    }
-  )
-);
+require('./passportLocalStrategy');
+require('./passportGoogleOAuth2Strategy');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
---
+++ config/passportLocalStrategy.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email' });
+        }
+        // Check if the password is correct
+        const pwIsValid = await bcrypt.compare(password, user.password);
+        if (!pwIsValid) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
