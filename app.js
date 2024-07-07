77625af7b875f7e6b2b695db0f5eb6837ccedcaf
Dallair220 <lovance777@gmail.com>
2024-07-07 20:55:11 +0200

    chore: Update app.js to add 'unsafe-eval' and 'unsafe-inline' to scriptSrc in Content-Security-Policy header

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-eval'", "'unsafe-inline'"],
       connectSrc: ["'self'", 'https://accounts.google.com'],
       imgSrc: [
         "'self'",
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,33 +2,6 @@
 const nextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: './dist', // Changes the build output directory to `./dist/`.
-
-  async headers() {
-    const cspHeader = `
-      default-src 'self';
-      script-src 'self' 'unsafe-eval' 'unsafe-inline';
-      style-src 'self' 'unsafe-inline';
-      img-src 'self' blob: data:;
-      font-src 'self';
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      upgrade-insecure-requests;
-    `;
-
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ' ').trim(),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
