822125147c8d6429f2ca8f7c12c8d3b712f70602
Dallair220 <lovance777@gmail.com>
2024-07-08 10:54:29 +0200

    chore: Update static file path for client in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -58,7 +58,7 @@ app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/players', playerRouter);
