1187bc57c4dd1d36e69b0c389e0405608e6fc07c
Dallair220 <lovance777@gmail.com>
2024-07-08 09:57:03 +0200

    chore: use npm instead of pnpm in /client

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 
+// For Heroku deployment
 app.set('trust proxy', 1);
 
 app.use(cors({ origin: 'http://localhost:3001' }));
