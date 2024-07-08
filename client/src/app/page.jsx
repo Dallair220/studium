af23f8efe85e139167f01d51a664a19afb84291b
Dallair220 <lovance777@gmail.com>
2024-07-08 19:12:44 +0200

    chore: Update page.jsx to use dynamic import for App component to disable SSR

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,7 +1,12 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import App from '../components/App';
 
+const DynamicApp = dynamic(() => import('../components/App'), {
+  ssr: false,
+});
+
 export default function Page() {
-  return <App />;
+  return <DynamicApp />;
 }
