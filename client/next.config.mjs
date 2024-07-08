4d04a1b3a9c1d3208378b55dbe7aadb65e9fe9b1
Dallair220 <lovance777@gmail.com>
2024-07-08 15:11:06 +0200

    Test for bugfix

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   distDir: './dist', // Changes the build output directory to `./dist/`.
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ddragon.leagueoflegends.com',
-        port: '',
-        pathname: '/cdn/14.13.1/img/profileicon/**',
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'ddragon.leagueoflegends.com',
+  //       port: '',
+  //       pathname: '/cdn/14.13.1/img/profileicon/**',
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
